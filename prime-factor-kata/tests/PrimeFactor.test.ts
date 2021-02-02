b1cb663ce671acea0c186629a2b94c0817de659c
Neto Costa <costa.neto@unesp.br>
2021-02-02 19:53:41 -0300

    First Test

--- prime-factor-kata/tests/PrimeFactor.test.ts
+++ prime-factor-kata/tests/PrimeFactor.test.ts
@@ -1,2 +1,8 @@
 import primeFactors from '../src/PrimeFactors';
 
+describe('prime factors', function(){
+    it('returns array',function(){
+        let result = primeFactors.generate(1);
+        expect(result).toBe([]);
+    })
+})
